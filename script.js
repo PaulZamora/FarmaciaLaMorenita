37692bcad6de693087b42ed4b9b85b8b70c47fdb
PaulZamora <60831525+PaulZamora@users.noreply.github.com>
2025-02-13 23:33:17 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const productos = document.querySelectorAll(".producto");
-    productos.forEach(producto => {
-        producto.addEventListener("click", () => {
-            alert(`Has seleccionado: ${producto.querySelector("h3").textContent}`);
-        });
-    });
-});
